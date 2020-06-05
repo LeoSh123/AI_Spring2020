357d0c7dd559a62c39e50d4ca9774099a1ce8a71
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2020-06-05 17:30:23 +0300

    Part B - Heuristic Function + set_game_param - implemented - NOT TESTED

--- HW2/MinimaxPlayer.py
+++ HW2/MinimaxPlayer.py
@@ -7,7 +7,12 @@ class MinimaxPlayer:
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
     def set_game_params(self, board):
-        pass
+        self.board = board
+        for i, row in enumerate(board):
+            for j, val in enumerate(row):
+                if val == 1:
+                    self.loc = (i, j)
+                    break
 
     def make_move(self, time):
         pass
@@ -15,5 +20,15 @@ class MinimaxPlayer:
     def set_rival_move(self, loc):
         pass
 
-    def Minimax_heuristic(self):
-        pass
+    def Minimax_heuristic(self, board, loc):
+        num_steps_available = 0
+        for d in self.directions:
+            i = loc[0] + d[0]
+            j = loc[1] + d[1]
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
+                num_steps_available += 1
+
+        if num_steps_available == 0:
+            return -1
+        else:
+            return 4 - num_steps_available
