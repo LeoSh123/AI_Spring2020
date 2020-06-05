6c2179dc396e9ddbabbebe16c6a7c71d48e1d167
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2020-06-05 16:27:15 +0300

    Part B - MinimaxPlayer class - Definitions

---
+++ HW2/MinimaxPlayer.py
@@ -0,0 +1,13 @@
+
+class MinimaxPlayer:
+    def __init__(self):
+        pass
+
+    def set_game_params(self, board):
+        pass
+
+    def make_move(self, time):
+        pass
+
+    def set_rival_move(self, loc):
+        pass
