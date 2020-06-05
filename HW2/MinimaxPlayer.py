3b0140e65641804624e22418b1d245f7e9d03969
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2020-06-05 16:39:22 +0300

    Part B - Heuristic Function - Definition

--- HW2/MinimaxPlayer.py
+++ HW2/MinimaxPlayer.py
@@ -1,7 +1,10 @@
+import time
 
 class MinimaxPlayer:
     def __init__(self):
-        pass
+        self.loc = None
+        self.board = None
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
     def set_game_params(self, board):
         pass
@@ -10,4 +13,7 @@ class MinimaxPlayer:
         pass
 
     def set_rival_move(self, loc):
+        pass
+
+    def Minimax_heuristic(self):
         pass
