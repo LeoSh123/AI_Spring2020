c402357154604269df51ee6f6230e9afa428f3c0
YotamPortal <44612484+YotamPortal@users.noreply.github.com>
2020-06-07 20:55:37 +0300

    Part B - DONE - need to implement new heuristic

--- HW2/MinimaxPlayer.py
+++ HW2/MinimaxPlayer.py
@@ -5,6 +5,9 @@ class MinimaxPlayer:
         self.loc = None
         self.board = None
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        self.make_move_flag = False
+        self.set_rival_move_flag = False
+        self.we_played_first = False
 
 
     def set_game_params(self, board):
@@ -16,6 +19,9 @@ class MinimaxPlayer:
                     break
 
     def make_move(self, time) -> (tuple):
+        if not self.set_rival_move_flag:
+            self.we_played_first = True
+            self.make_move_flag = True
         ID_start_time = T.time()
         depth = 1
 
@@ -36,14 +42,23 @@ class MinimaxPlayer:
             next_iteration_time = self.time_bound(numOfNodes, last_iteration_time, depth)
             time_until_now = T.time() - ID_start_time
 
+        self.board[self.loc] = -1
+        self.board[move] = 1
+        self.loc = move
+
         return x, y
 
     def set_rival_move(self, loc):
+        if not self.make_move_flag:
+            self.set_rival_move_flag = True
         self.board[self.getLoc(self.board, 2)] = -1
         self.board[loc] = 2
 
     # TODO: Implement a new one
-    def Minimax_heuristic(self, board, loc):
+    def Minimax_heuristic(self, board, loc, agentTurn):
+        flag, res = self.is_final(board, agentTurn)
+        if flag:
+            return res
         num_steps_available = 0
         for d in self.directions:
             i = loc[0] + d[0]
@@ -58,7 +73,7 @@ class MinimaxPlayer:
 
     def Minimax(self, board, depth: int, agent: int, loc: tuple) -> (tuple, int, int):
         if depth == 0:
-            return loc, 0, self.Minimax_heuristic(board, self.getLoc(board, 1))
+            return loc, 0, self.Minimax_heuristic(board, self.getLoc(board, 1), agent)
 
         CurNumOfNodes = 0
 
@@ -69,8 +84,8 @@ class MinimaxPlayer:
             CurMaxLoc = None
             CurNumOfNodes = len(list_of_neighbors)
             for child in list_of_neighbors:
-                temp_board = board
-                temp_board[loc] = -1
+                temp_board = board.copy()
+                temp_board[agent_loc] = -1
                 temp_board[child] = 1
                 res_loc, res_num_of_nodes, res_value = self.Minimax(temp_board, depth - 1, 2, child)
                 if CurMax < res_value:
@@ -86,8 +101,8 @@ class MinimaxPlayer:
             CurMinLoc = None
             CurNumOfNodes = len(list_of_neighbors)
             for child in list_of_neighbors:
-                temp_board = board
-                temp_board[loc] = -1
+                temp_board = board.copy()
+                temp_board[agent_loc] = -1
                 temp_board[child] = 2
                 res_loc, res_num_of_nodes, res_value = self.Minimax(temp_board, depth - 1, 1, child)
                 if CurMin > res_value:
@@ -122,3 +137,32 @@ class MinimaxPlayer:
             for j, val in enumerate(row):
                 if val == agent:
                     return i, j
+
+    def is_final(self, board, agentTurn):
+        agent_1 = self.getLoc(board, 1)
+        agent_2 = self.getLoc(board, 2)
+
+        list_of_neighbors_1 = self.succ(board, agent_1)
+        list_of_neighbors_2 = self.succ(board, agent_2)
+
+        if len(list_of_neighbors_1) == 0:
+            return True, -2
+
+        else:
+            if len(list_of_neighbors_2) == 0:
+                if agentTurn == 1:
+                    if not self.we_played_first:
+                       for child in list_of_neighbors_1:
+                           list_of_child_neighbors = self.succ(board, child)
+                           if len(list_of_child_neighbors) != 0:
+                               return True, 5
+                           else:
+                               return True, -2
+
+                    else:
+                        return True, 5
+
+                else:
+                    return True, 5
+            else:
+                return False, False
