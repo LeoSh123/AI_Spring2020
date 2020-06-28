8ee0c78e9793a6a45964ea349d19923a9e959ce1
Leonid <leonidshleifer@gmail.com>
2020-06-28 19:51:39 +0300

    ex1-3

--- HW3/DT.py
+++ HW3/DT.py
@@ -9,15 +9,15 @@ train_df = pd.read_csv("train.csv")
 dataTop = train_df.head()
 
 
-def getMaxInCol(numOfCol)->(float):
-    maxVal = max(val for val in train_df.iloc[:,numOfCol])
+def getMaxInCol(feature:str ,dataFrame:pd.DataFrame)->(float):
+    maxVal = max(val for val in dataFrame[feature])
     return maxVal
 
-def getMinInCol(numOfCol)->(float):
-    minVal = min(val for val in train_df.iloc[:,numOfCol])
+def getMinInCol(feature:str ,dataFrame:pd.DataFrame)->(float):
+    minVal = min(val for val in dataFrame[feature])
     return minVal
 
-def getEntropy(indexArray: np.array, dataFrame:train_df)->(float):
+def getEntropy(dataFrame:pd.DataFrame)->(float):
     # average = (getMaxInCol(numOfFeature) + getMinInCol(numOfFeature)) / 2
     # array = np.array(train_df.iloc[:,numOfFeature])
     # lowerIndexArray = np.argwhere[array < average]
@@ -33,20 +33,111 @@ def getEntropy(indexArray: np.array, dataFrame:train_df)->(float):
     #     index +=1
     # listOfAboveIndex = train_df.index - listOfBelowIndex
     positiveSum = 0
-    for index in indexArray:
-        if dataFrame.iloc[index , 0] == 1:
+
+    for val in dataFrame['diagnosis']:
+        if val == 1:
             positiveSum += 1
-    negativeSum = len(indexArray) - positiveSum
-    probZero = negativeSum / len(indexArray)
-    probOne = positiveSum / len(indexArray)
-    return -1*(probZero * math.log2(probZero) + probOne * math.log2(probOne))
+    NumOfVals = len(dataFrame['diagnosis'])
+    negativeSum = NumOfVals - positiveSum
+    probZero = negativeSum / NumOfVals
+    probOne = positiveSum / NumOfVals
+    if probZero == 0:
+        v0 = 0
+    else:
+        v0 = probZero * math.log2(probZero)
+    if probOne == 0:
+        v1 = 0
+    else:
+        v1 = probOne * math.log2(probOne)
+
+    return -1*(v0 + v1)
 
 
 
 
 
-array = np.array(train_df.index)
-print(array)
-print("entropy is", getEntropy(array, train_df))
+
+
+def MajorityClass(dataFrame: pd.DataFrame)->(bool):
+    positive = 0
+    negative = 0
+    for case in dataFrame.iloc[:,0]:
+        if case == 0:
+            negative += 1
+        else:
+            positive += 1
+    if positive > negative:
+        return True
+    else:
+        return False
+
+
+def SelectFeature(dataFrame: pd.DataFrame)->(str):
+     CurrentEntropy = getEntropy(dataFrame)
+     maxIG = 0
+     for col in dataFrame:
+         if col == 'diagnosis':
+             continue
+
+
+         average =  (getMinInCol(col, dataFrame) + getMaxInCol(col, dataFrame)) /2
+         newDataFrameAbove = dataFrame.copy()
+         newDataFrameBelow = dataFrame.copy()
+
+         aboveIndex = newDataFrameAbove[newDataFrameAbove[col] > average].index
+         belowIndex = newDataFrameAbove[newDataFrameAbove[col] <= average].index
+
+         newDataFrameAbove.drop(belowIndex, inplace=True)
+         newDataFrameBelow.drop(aboveIndex, inplace=True)
+
+         aboveLen = len(aboveIndex)
+         belowLen = len(belowIndex)
+         totalLen = aboveLen + belowLen
+
+         IG = CurrentEntropy - ((aboveLen/totalLen)*getEntropy(newDataFrameAbove) + (belowLen/totalLen)*getEntropy(newDataFrameBelow))
+         maxIG = max(IG, maxIG)
+
+     for col in dataFrame:
+         if col == 'diagnosis':
+             continue
+
+         average = (getMinInCol(col, dataFrame) + getMaxInCol(col, dataFrame)) / 2
+         newDataFrameAbove = dataFrame.copy()
+         newDataFrameBelow = dataFrame.copy()
+
+         aboveIndex = newDataFrameAbove[newDataFrameAbove[col] > average].index
+         belowIndex = newDataFrameAbove[newDataFrameAbove[col] <= average].index
+
+         newDataFrameAbove.drop(belowIndex, inplace=True)
+         newDataFrameBelow.drop(aboveIndex, inplace=True)
+
+         aboveLen = len(aboveIndex)
+         belowLen = len(belowIndex)
+         totalLen = aboveLen + belowLen
+
+         IG = CurrentEntropy - ((aboveLen / totalLen) * getEntropy(newDataFrameAbove) + (belowLen / totalLen) * getEntropy(
+             newDataFrameBelow))
+         if IG == maxIG:
+             return col
+
+
+
+
+
+
+# array = np.array(train_df.index)
+# print(array)
+# print("entropy is", getEntropy(train_df))
+# print(train_df['area_mean'])
+# print("Max area mean:" , getMaxInCol('area_mean',train_df))
+# print("Min area mean:" , getMinInCol('area_mean',train_df))
+
+
+print(SelectFeature(train_df))
+
+
+
+
+
 
 
